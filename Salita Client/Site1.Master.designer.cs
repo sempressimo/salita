d65ac3d6cc2a14174b58346d192265a70d64e5db
sempressimo <sempressimo@hotmail.com>
2015-06-27 18:32:58 -0400

    Scripts at end of page for performance.

--- Salita Client/Site1.Master.designer.cs
+++ Salita Client/Site1.Master.designer.cs
@@ -101,5 +101,14 @@ namespace Salita_Client {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// ScriptSection control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ScriptSection;
     }
 }
