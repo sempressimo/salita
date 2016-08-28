ff3669d78e6a99f9b7605a9a534dbc8e8169f476
sempressimo <sempressimo@hotmail.com>
2016-08-28 17:52:44 -0400

    Added clear salita page

---
+++ Salita Client/clear_salita.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="clear_salita.aspx.cs" Inherits="Salita_Client.clear_salita" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ScriptSection" runat="server">
+</asp:Content>
---
+++ Salita Client/clear_salita.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Salita_Client
+{
+    public partial class clear_salita : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                LeaveLounge();
+            }
+            catch (Exception E)
+            {
+                Response.Write(E.Message);
+            }
+        }
+
+        public void LeaveLounge()
+        {
+            SalitaEntities db = new SalitaEntities();
+
+            //
+            // Cancel requests except for transportation
+            //
+            var Needs = db.CustomerNeeds.Where(p => p.WasFullfilled == false && p.Canceled == false && p.RequestedService_ID != 3 && p.RequestedService_ID != 4);
+
+            foreach (CustomerNeed N in Needs)
+            {
+                SalitaEntities db2 = new SalitaEntities();
+
+                var NeedToUpdate = db2.CustomerNeeds.Single(p => p.CustomerNeed_ID == N.CustomerNeed_ID);
+
+                NeedToUpdate.Canceled = true;
+
+                db2.SaveChanges();
+            }
+
+            //
+            // Take customer out
+            //
+            var V = db.Visits.SingleOrDefault(p => p.InLounge == true);
+
+            if (V != null)
+            {
+                V.InLounge = false;
+
+                db.SaveChanges();
+            }
+        }
+    }
+}
